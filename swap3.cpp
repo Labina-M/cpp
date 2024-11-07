8aad36818dcc991d7a0155f077318a7f530c6695
Malik Labina <156318986+Labina-M@users.noreply.github.com>
2024-11-07 21:45:52 +0530

    Create swap3.cpp

---
+++ swap3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a=5;
+int b=9;
+int temp;
+cout<<"before swapping ";
+cout<<a<<" "<<b;
+cout<<endl;
+
+
+a=a+b;
+b=a-b;
+a=a-b;
+cout<<"after swapping ";
+cout<<a<<" "<<b;
+
+return 0;
+}
