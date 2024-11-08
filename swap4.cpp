63e6dd7d679ea613ed642a08aedd758a100fa60c
Malik Labina <156318986+Labina-M@users.noreply.github.com>
2024-11-08 12:39:08 +0530

    Create swap4.cpp

---
+++ swap4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void swapUsingStack(int &a, int &b) {
+    stack<int> s;
+
+    // Push both numbers onto the stack
+    s.push(a);
+    s.push(b);
+
+    // Pop them back in swapped order
+    a = s.top();
+    s.pop();
+    b = s.top();
+    s.pop();
+}
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+
+    cout << "Before swapping:" << endl;
+    cout << "num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    swapUsingStack(num1, num2);
+
+    cout << "After swapping:" << endl;
+    cout << "num1 = " << num1 << ", num2 = " << num2 << endl;
+
+    return 0;
+}
